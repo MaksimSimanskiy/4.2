bb187f4c41ee0fd0a4247545e27e7c44412d4634
MaksimSimanskiy <78483044+MaksimSimanskiy@users.noreply.github.com>
2022-03-21 12:26:44 +0300

    Add files via upload

--- individual2.py
+++ individual2.py
@@ -1,64 +1,91 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from random import randint
 
 """
-Создать класс BitString для работы с битовыми строками не более чем из 100 бит. Битовая
-строка должна быть представлена списком типа int, каждый элемент которого принимает
-значение 0 или 1. Реальный размер списка задается как аргумент конструктора
-инициализации. Должны быть реализованы все традиционные операции для работы с
-битовыми строками: and, or, xor, not. Реализовать сдвиг влево и сдвиг вправо на заданное
-количество битов.
+Нагрузка преподавателя за учебный год представляет собой список дисциплин,
+преподаваемых им в течение года. Одна дисциплина представляется информационным
+словарем с ключами: название дисциплины, семестр проведения, количество студентов,
+количество часов аудиторных лекций, количество аудиторных часов практики, вид контроля
+(зачет или экзамен). Реализовать класс WorkTeacher, моделирующий бланк назначенной
+преподавателю нагрузки. Класс содержит фамилию преподавателя, дату утверждения,
+список преподаваемых дисциплин, объем полной нагрузки в часах и в ставках. Дисциплины
+в списке не должны повторяться. Объем в ставках вычисляется как частное от деления
+объема в часах на среднюю годовую ставку, одинаковую для всех преподавателей кафедры.
+Элемент списка преподаваемых дисциплин содержит дисциплину, количество часов,
+выделяемых на зачет (0,35 ч на одного студента) или экзамен (0,5 ч на студента), сумму
+часов по дисциплине. Реализовать добавление и удаление дисциплин; вычисление
+суммарной нагрузки в часах и ставках. Должен осуществляться контроль за превышением
+нагрузки более полутора ставок.
+
 """
 
-class BitString:
+class WorkTeacher:
 
-    def __init__(self, other=10):
-        CONST_SIZE = 100
-        self.other = other
-        if CONST_SIZE > self.other:
-            self.b = [randint(0, 1) for i in range(self.other)]
-            self.number = int(''.join(map(str, self.b)))
+    def __init__(self, sname, date, discipline):
+        sgs = 100
+        self.sname = sname
+        self.date = date
+        self.discipline = self.uniq(discipline)
+        self.load = self.all_hours()
+        self.stavka = self.load / sgs
+        if (self.stavka > 1.5):
+            print("Нагрузка превышена")
 
-    def display(self):
-        print(f"Битовое число {self.number}")
+    # проверка на уникальность
+    def uniq(self, discipline):
+        unique = []
+        for number in discipline:
+            if number not in unique:
+                unique.append(number)
+        return unique
+
+    def all_hours(self):
+        all_hours = 0
+        for i in self.discipline:
+            all = i['hours_pract'] + i['hours_lekt']
+            i['summ_hours'] = all
+            all_hours += all
+            if i['cont'] == 'exam':
+                i['hours_for_student'] = 0.5
+            else:
+                i['hours_for_student'] = 0.35
+        return all_hours
 
     def __getitem__(self, key):
-        return self.number[key]
+        return self.discipline[key]
 
     def __setitem__(self, key, value):
-        self.number[key] = value
-        return self.number
+        self.discipline[key] = value
+        return self.discipline
 
     def size(self):
-        print(f"Длина числа {self.other}")
+        return len(self.discipline)
 
-    def __and__(self, other):
-        return self.number & other.number
+    def display(self):
+        print(f"Список предметов {self.discipline}")
 
-    def __xor__(self, other):
-        return self.number ^ other.number
+    def add(self, other):
+        self.discipline.append(other)
+        self.all_hours()
+        return self.discipline
 
-    def __or__(self, other):
-        return self.number | other.number
-
-    def __lshift__(self, other):
-        return self.number << other
-
-    def __rshift__(self, other):
-        return self.number >> other
-
-    def __invert__(self):
-        return ~self.number
-
+    def remove(self, other):
+        self.discipline.remove(other)
+        return self.discipline
 
 if __name__ == '__main__':
-    new = BitString(15)
-    new2 = BitString(15)
-    print(new & new2)
-    print(new >> 4)
-    print(new << 4)
-    print(new | new2)
-    new.size()
-    new.display()
-    new2.display()
+    math = {'name': "Algebra", 'term': 2, 'students': 45,
+            'hours_lekt': 20, 'hours_pract': 40, 'cont': 'exam'}
+    rus = {'name': "Ruskiy", 'term': 1, 'students': 80,
+            'hours_lekt': 10, 'hours_pract': 30, 'cont': 'zachet'}
+    litra = {'name': "Literatura", 'term': 3, 'students': 50,
+           'hours_lekt': 20, 'hours_pract': 25, 'cont': 'zachet'}
+    fizra = {'name': "Fizcultura", 'term': 1, 'students': 60,
+             'hours_lekt': 10, 'hours_pract': 35, 'cont': 'zachet'}
+    list_sub = [math, rus, litra]
+    a = WorkTeacher('Сергей', '25/24/22', list_sub)
+    a.add(fizra)
+    a.display()
+    print(a.size())
+    a.remove(litra)
+    a.display()
